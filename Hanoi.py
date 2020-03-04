93c49b89bcfa34d9b93e81b665c632ada6b4c746
thefrollickingnerd <jack.moran192@gmail.com>
2020-03-05 09:54:16 +1030

    Added disk adding and subtracting functionality to pole class and ability to find op disk.

--- Hanoi.py
+++ Hanoi.py
@@ -3,9 +3,17 @@ class Pole:
     def __init__(self,label,disks):
         self.label = label
         self.disks = disks
-# class Disk:
-#     def __init__(self,size):
-#         self.size = size 
+    
+    def addto(self,newdisk):
+        self.disks = disks.append(newdisk)
+    
+    def subtractfrom(self):
+        del disks[-1]
+    
+    def topdisk(self):
+        topdisk = self.disks[-1]
+        return topdisk
+
 
 import random
 import numpy as np 
@@ -21,12 +29,24 @@ def towergenerator():
     global A,B,C
     numdisks   = random.randint(0,20)                   
     disks  = random.sample(range(1,100),numdisks)   
-    disks.sort()
+    disks.sort(reverse = True)
     A = Pole('A',disks)
     B = Pole('B',[])
     C = Pole('C',[])    
+
+def move(n,f,t):
+    #Function move disk from f -> t  
     
-        
+
+def solver(n,f,h,t):
+    #n:no. of disks, f:from, h:helper, t:target
+    if n == 0:
+        pass
+    else:
+        hanoi(n-1,f,t,h)
+        move(n,f,t)
+        hanoi(n-1,h,f,t)    
+
 towergenerator()
 print(A.disks)
 
